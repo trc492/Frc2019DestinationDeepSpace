0db84133bce504eaf36b1e9165c90f635f2cd16c
Abhay D <a.deshpande012@gmail.com>
2019-01-22 16:56:36 -0800

    Added outline for joystic control in FrcAuto

--- robot/src/main/java/team492/FrcAuto.java
+++ robot/src/main/java/team492/FrcAuto.java
@@ -25,6 +25,7 @@ package team492;
 import common.CmdPidDrive;
 import common.CmdTimedDrive;
 import frclib.FrcChoiceMenu;
+import frclib.FrcJoystick;
 import hallib.HalDashboard;
 import trclib.TrcRobot;
 import trclib.TrcRobot.RunMode;
@@ -37,12 +38,7 @@ public class FrcAuto implements TrcRobot.RobotMode
 
     public static enum AutoStrategy
     {
-        X_TIMED_DRIVE,
-        Y_TIMED_DRIVE,
-        X_DISTANCE_DRIVE,
-        Y_DISTANCE_DRIVE,
-        TURN_DEGREES,
-        DO_NOTHING
+        X_TIMED_DRIVE, Y_TIMED_DRIVE, X_DISTANCE_DRIVE, Y_DISTANCE_DRIVE, TURN_DEGREES, DO_NOTHING
     } // enum AutoStrategy
 
     private Robot robot;
@@ -53,7 +49,8 @@ public class FrcAuto implements TrcRobot.RobotMode
     private FrcChoiceMenu<AutoStrategy> autoStrategyMenu;
     private AutoStrategy autoStrategy;
     private double delay;
-    
+    private boolean manualOverride = false;
+
     private TrcRobot.RobotCommand autoCommand;
 
     public FrcAuto(Robot robot)
@@ -84,9 +81,22 @@ public class FrcAuto implements TrcRobot.RobotMode
         final String funcName = moduleName + ".startMode";
 
         robot.getGameInfo();
-        robot.globalTracer.traceInfo(funcName, "%s_%s%03d (%s%d) [FMSConnected=%b] msg=%s",
-            robot.eventName, robot.matchType, robot.matchNumber, robot.alliance.toString(), robot.location,
-            robot.ds.isFMSAttached(), robot.gameSpecificMessage);
+        robot.globalTracer
+            .traceInfo(funcName, "%s_%s%03d (%s%d) [FMSConnected=%b] msg=%s", robot.eventName, robot.matchType,
+                robot.matchNumber, robot.alliance.toString(), robot.location, robot.ds.isFMSAttached(),
+                robot.gameSpecificMessage);
+
+        //
+        // Configure joysticks.
+        //
+        robot.leftDriveStick.setButtonHandler(this::leftDriveStickButtonEvent);
+        robot.leftDriveStick.setYInverted(true);
+
+        robot.rightDriveStick.setButtonHandler(this::rightDriveStickButtonEvent);
+        robot.rightDriveStick.setYInverted(true);
+
+        robot.operatorStick.setButtonHandler(this::operatorStickButtonEvent);
+        robot.operatorStick.setYInverted(false);
 
         robot.encoderXPidCtrl.setOutputLimit(RobotInfo.DRIVE_MAX_XPID_POWER);
         robot.encoderYPidCtrl.setOutputLimit(RobotInfo.DRIVE_MAX_YPID_POWER);
@@ -107,18 +117,18 @@ public class FrcAuto implements TrcRobot.RobotMode
                 break;
 
             case X_DISTANCE_DRIVE:
-                autoCommand = new CmdPidDrive(
-                    robot, robot.pidDrive, delay, robot.driveDistance, 0.0, 0.0, robot.drivePowerLimit, false);
+                autoCommand = new CmdPidDrive(robot, robot.pidDrive, delay, robot.driveDistance, 0.0, 0.0,
+                    robot.drivePowerLimit, false);
                 break;
 
             case Y_DISTANCE_DRIVE:
-                autoCommand = new CmdPidDrive(
-                    robot, robot.pidDrive, delay, 0.0, robot.driveDistance, 0.0, robot.drivePowerLimit, false);
+                autoCommand = new CmdPidDrive(robot, robot.pidDrive, delay, 0.0, robot.driveDistance, 0.0,
+                    robot.drivePowerLimit, false);
                 break;
 
             case TURN_DEGREES:
-                autoCommand = new CmdPidDrive(
-                    robot, robot.pidDrive, delay, 0.0, 0.0, robot.turnDegrees, robot.drivePowerLimit, false);
+                autoCommand = new CmdPidDrive(robot, robot.pidDrive, delay, 0.0, 0.0, robot.turnDegrees,
+                    robot.drivePowerLimit, false);
                 break;
 
             case DO_NOTHING:
@@ -141,6 +151,14 @@ public class FrcAuto implements TrcRobot.RobotMode
             robot.updateDashboard(RunMode.AUTO_MODE);
             robot.announceSafety();
         }
+
+        if (manualOverride)
+        {
+            double x = robot.leftDriveStick.getXWithDeadband(true);
+            double y = robot.rightDriveStick.getYWithDeadband(true);
+            double rot = robot.rightDriveStick.getTwistWithDeadband(true);
+            robot.driveBase.holonomicDrive(x, y, rot);
+        }
     } // runPeriodic
 
     @Override
@@ -158,5 +176,132 @@ public class FrcAuto implements TrcRobot.RobotMode
             }
         }
     } // runContinuous
+
+    //
+    // Implements TrcJoystick.ButtonHandler.
+    //
+
+    public void leftDriveStickButtonEvent(int button, boolean pressed)
+    {
+        robot.dashboard.displayPrintf(8, " LeftDriveStick: button=0x%04x %s", button, pressed ? "pressed" : "released");
+
+        switch (button)
+        {
+            case FrcJoystick.LOGITECH_TRIGGER:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON2:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON3:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON4:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON5:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON6:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON7:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON8:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON9:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON10:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON11:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON12:
+                break;
+        }
+    } // leftDriveStickButtonEvent
+
+    public void rightDriveStickButtonEvent(int button, boolean pressed)
+    {
+        robot.dashboard.displayPrintf(8, "RightDriveStick: button=0x%04x %s", button, pressed ? "pressed" : "released");
+
+        switch (button)
+        {
+            case FrcJoystick.SIDEWINDER_TRIGGER:
+                break;
+
+            case FrcJoystick.SIDEWINDER_BUTTON2:
+                break;
+
+            case FrcJoystick.SIDEWINDER_BUTTON3:
+                break;
+
+            case FrcJoystick.SIDEWINDER_BUTTON4:
+                break;
+
+            case FrcJoystick.SIDEWINDER_BUTTON5:
+                break;
+
+            case FrcJoystick.SIDEWINDER_BUTTON6:
+                break;
+
+            case FrcJoystick.SIDEWINDER_BUTTON7:
+                break;
+
+            case FrcJoystick.SIDEWINDER_BUTTON8:
+                break;
+
+            case FrcJoystick.SIDEWINDER_BUTTON9:
+                break;
+        }
+    } // rightDriveStickButtonEvent
+
+    public void operatorStickButtonEvent(int button, boolean pressed)
+    {
+        robot.dashboard.displayPrintf(8, "  OperatorStick: button=0x%04x %s", button, pressed ? "pressed" : "released");
+
+        switch (button)
+        {
+            case FrcJoystick.LOGITECH_TRIGGER:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON2:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON3:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON4:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON5:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON6:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON7:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON8:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON9:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON10:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON11:
+                break;
+
+            case FrcJoystick.LOGITECH_BUTTON12:
+                break;
+        }
+    } // operatorStickButtonEvent
 
 } // class FrcAuto
