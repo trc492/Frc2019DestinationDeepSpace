e4556944036e3580e3da35c083f82339f707cf85
DrDab <duvictor514@gmail.com>
2019-02-09 11:04:36 -0800

    Fixed formatting on LineFollowingUtils

--- robot/src/main/java/team492/LineFollowingUtils.java
+++ robot/src/main/java/team492/LineFollowingUtils.java
@@ -3,25 +3,25 @@ package team492;
 public class LineFollowingUtils
 {
     public static final int PIXY2_HEIGHT = 104;
-	public static final int PIXY2_WIDTH = 636;
-	
+    public static final int PIXY2_WIDTH = 636;
+
     public static double getTurnDegrees(double angle)
     {
         return angle - 90.0;
     }
-	
-	public static double getAngle(RealWorldPair origin, RealWorldPair p2) 
-	{
-		double dx = p2.getXLength() - origin.getXLength();
-		double dy = p2.getYLength() - origin.getYLength();
-				
-		double theta = Math.atan2(dy, dx);
-		theta = Math.toDegrees(theta);
-		theta = (theta + 360.0) % 360.0;
-		return theta;
-	}
-	
-	/**
+
+    public static double getAngle(RealWorldPair origin, RealWorldPair p2)
+    {
+        double dx = p2.getXLength() - origin.getXLength();
+        double dy = p2.getYLength() - origin.getYLength();
+
+        double theta = Math.atan2(dy, dx);
+        theta = Math.toDegrees(theta);
+        theta = (theta + 360.0) % 360.0;
+        return theta;
+    }
+
+    /**
 	 * This function creates a rough estimation of an object's position in real-world units.
      * 
 	 * @param x0 the x-coordinate of a pair on the camera plane. (x: right positive)
@@ -30,28 +30,29 @@ public class LineFollowingUtils
 	 * @param heightCoefficient the height coefficient, the height of the camera view width in inches.
 	 * @return a RealWorldPair instance of the approximate scaled real world location of the objects at the coordinates (x0, y0)
 	 */
-	public static RealWorldPair getRWP(int x0, int y0, double widthCoefficient, double heightCoefficient)
-	{
-		double xLength = widthCoefficient * ((double)x0 / (double)PIXY2_WIDTH);
-		double yLength = heightCoefficient * ((double)(PIXY2_HEIGHT - y0) / (double)PIXY2_HEIGHT);
-		return new RealWorldPair(xLength, yLength);
+    public static RealWorldPair getRWP(int x0, int y0, double widthCoefficient, double heightCoefficient)
+    {
+        double xLength = widthCoefficient * ((double) x0 / (double) PIXY2_WIDTH);
+        double yLength = heightCoefficient * ((double) (PIXY2_HEIGHT - y0) / (double) PIXY2_HEIGHT);
+        return new RealWorldPair(xLength, yLength);
     }
-    
+
     public static class RealWorldPair
     {
         private double xLength;
         private double yLength;
+
         public RealWorldPair(double xLength, double yLength)
         {
             this.xLength = xLength;
             this.yLength = yLength;
         }
-        
+
         public double getXLength()
         {
             return xLength;
         }
-        
+
         public double getYLength()
         {
             return yLength;
